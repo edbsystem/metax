c1cf86aefbc2244108bec04bcbfed5b6bb1b23b8
edbsystem <thomas@edbsystem.dk>
2021-03-01 15:38:01 +0100

    01-03-2021 15:37

--- arkiveringsversioner/views/soeg_view.py
+++ arkiveringsversioner/views/soeg_view.py
@@ -38,43 +38,29 @@ def soeg_view(request):
     _svar_fra = request.GET.get('svar_fra_soeg') if 'svar_fra_soeg' in request.GET and request.GET.get('svar_fra_soeg') != '' else None
     _svar_til = request.GET.get('svar_til_soeg') if 'svar_til_soeg' in request.GET and request.GET.get('svar_til_soeg') != '' else None
 
-    # print('avid_soeg:', _avid)
-    # print('jnr_soeg:', _jnr)
-    # print('titel_soeg:', _titel)
-    # print('land_soeg:', _land)
-    # print('status_soeg:', _status)
-    # print('kategori_soeg:', _kategori)
-    # print('klassifikation_soeg:', _klassifikation)
-    # print('type_soeg:', _type)
-    # print('arkivar_soeg:', _arkivar)
-    # print('leverandoer_soeg:', _leverandoer)
-    # print('tester_soeg:', _tester)
-    # print('afleveringsfrist_fra_soeg:', _afleveringsfrist_fra)
-    # print('afleveringsfrist_til_soeg:', _afleveringsfrist_til)
-    # print('modtaget_fra_soeg:', _modtaget_fra)
-    # print('modtaget_til_soeg:', _modtaget_til)
-    # print('adgang_fra_soeg:', _adgang_fra)
-    # print('adgang_til_soeg:', _adgang_til)
-    # print('svarfrist_fra_soeg:', _svarfrist_fra)
-    # print('svarfrist_til_soeg:', _svarfrist_til)
-    # print('genafleveringsfrist_fra_soeg:', _genafleveringsfrist_fra)
-    # print('genafleveringsfrist_til_soeg:', _genafleveringsfrist_til)
-    # print('svar_fra_soeg:', _svar_fra)
-    # print('svar_til_soeg:', _svar_til)
-
     _avs_objs = Arkiveringsversion.objects.all()
-    _avs_objs = _avs_objs.filter(avid__icontains=_avid) if _avid else _avs_objs
-    _avs_objs = _avs_objs.filter(jnr__icontains=_jnr) if _jnr else _avs_objs
-    _avs_objs = _avs_objs.filter(titel__icontains=_titel) if _titel else _avs_objs
-    _avs_objs = _avs_objs.filter(qtq([Q(land=value) for value in _land])) if _land else _avs_objs
-    _avs_objs = _avs_objs.filter(qtq([Q(kategori=value) for value in _kategori])) if _kategori else _avs_objs
-    _avs_objs = _avs_objs.filter(qtq([Q(klassifikation=value) for value in _klassifikation])) if _klassifikation else _avs_objs
-    _avs_objs = _avs_objs.filter(qtq([Q(type=value) for value in [Type.objects.get(navn=value) for value in _type]])) if _type else _avs_objs
+    if _avid:
+        _avs_objs = _avs_objs.filter(avid__icontains=_avid)
+    if _jnr:
+        _avs_objs = _avs_objs.filter(jnr__icontains=_jnr)
+    if _titel:
+        _avs_objs = _avs_objs.filter(titel__icontains=_titel)
+    if _land:
+        _avs_objs = _avs_objs.filter(qtq([Q(land=value) for value in _land]))
+    if _kategori:
+        _avs_objs = _avs_objs.filter(qtq([Q(kategori=value) for value in _kategori]))
+    if _klassifikation:
+        _avs_objs = _avs_objs.filter(qtq([Q(klassifikation=value) for value in _klassifikation]))
+    if _type:
+        _avs_objs = _avs_objs.filter(qtq([Q(type=value) for value in [Type.objects.get(navn=value) for value in _type]]))
 
     _version_objs = Version.objects.filter(avid__in=(av for av in _avs_objs))
-    _version_objs = _version_objs.filter(qtq([Q(status=value) for value in _status])) if _status else _version_objs
-    _version_objs = _version_objs.filter(qtq([Q(arkivar=Bruger.objects.get(profil=value)) for value in [Profil.objects.get(initialer=value) for value in _arkivar]])) if _arkivar else _version_objs
-    _version_objs = _version_objs.filter(qtq([Q(tester=Bruger.objects.get(profil=value)) for value in [Profil.objects.get(initialer=value) for value in _tester]])) if _tester else _version_objs
+    if _status:
+        _version_objs = _version_objs.filter(qtq([Q(status=value) for value in _status]))
+    if _arkivar:
+        _version_objs = _version_objs.filter(qtq([Q(arkivar=Bruger.objects.get(profil=value)) for value in [Profil.objects.get(initialer=value) for value in _arkivar]]))
+    if _tester:
+        _version_objs = _version_objs.filter(qtq([Q(tester=Bruger.objects.get(profil=value)) for value in [Profil.objects.get(initialer=value) for value in _tester]]))
 
     return render(request, 'arkiveringsversioner/soeg.html', {
         "bruger_rettigheder": rettigheder(request.user),
